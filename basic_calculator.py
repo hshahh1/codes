fb6a47e29c0a1462dc2c5bae527795f4de6b17a7
hs <hshahh1127@gmail.com>
2023-09-15 17:33:35 -0400

    first commit

---
+++ basic_calculator.py
@@ -0,0 +1,45 @@
+# Define the calculator functions
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y == 0:
+        return "Cannot divide by zero!"
+    return x / y
+
+# Main calculator loop
+while True:
+    # Display menu options
+    print("Options:")
+    print("Enter 'add' for addition")
+    print("Enter 'subtract' for subtraction")
+    print("Enter 'multiply' for multiplication")
+    print("Enter 'divide' for division")
+    print("Enter 'quit' to end the program")
+
+    # Get user input
+    user_input = input(": ")
+
+    # Check if the user wants to quit
+    if user_input == "quit":
+        break
+
+    # Check for valid operations
+    if user_input in ("add", "subtract", "multiply", "divide"):
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+
+        if user_input == "add":
+            print("Result:", add(num1, num2))
+        elif user_input == "subtract":
+            print("Result:", subtract(num1, num2))
+        elif user_input == "multiply":
+            print("Result:", multiply(num1, num2))
+        elif user_input == "divide":
+            print("Result:", divide(num1, num2))
